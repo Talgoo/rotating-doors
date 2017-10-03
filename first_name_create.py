3a12f5bc45a3737ad6960cf1f20031b6d6b9cb38
Ron <ronlitman23@gmail.com>
2017-10-03 16:08:38 +0300

    unfinished script for retrieving a family name database from Beit Hatfutsot

--- first_name_create.py
+++ first_name_create.py
@@ -1,28 +1,37 @@
 # -- coding: utf-8 --
 import requests
 import csv
-
+import json
+import pprint
 
 # CONSTS
 url = 'https://api.dbs.bh.org.il/v1/search?'
 total_count = 9985
 
 def requests_first_name(n):
-    raw_names = []
+    clean_names = []
     payload = {'collection': 'familyNames', 'from_': str(n), 'q': '*' }
     r = requests.get(url, params=payload)
-    for line in r:
-        if '"He":' in line:
-            raw_names.append(line)
-    return raw_names
+    j = json.loads(r.text)
+    for row in j['hits']['hits']:
+        for line in row:
+            if line == '_source':
+                for leg in row[line]:
+                    if leg == 'Header':
+                        clean_names.append(row[line][leg].values())
+    return clean_names
 
+def write_to_files(clean_names):
+    with open('first_name_list.csv', 'w') as f:
+        w = csv.writer(f)
+        w.writerow(["English", "Hebrew"])  # field header
+        w.writerows([list(name) for name in clean_names])
 
 def main():
-    raw_names = []
+    clean_names = []
     for i in range(0, total_count, 15):
-        raw_names.append(requests_first_name(i))
-        pass
-
+        clean_names.append(requests_first_name(i))
+    write_to_files(clean_names)
 
 
 if __name__ == '__main__':
