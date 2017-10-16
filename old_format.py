450d21d7c35601b800afdf0be298f5dc08034fe4
Ron <ronlitman23@gmail.com>
2017-10-16 21:00:46 +0300

    unfinish script that transfore rtp files to csv

---
+++ old_format.py
@@ -0,0 +1,53 @@
+# -- coding: utf-8 --
+import csv
+import glob2
+import os
+from collections import defaultdict
+from collections import namedtuple
+from pyth.plugins.rtf15.reader import Rtf15Reader
+from pyth.plugins.plaintext.writer import PlaintextWriter
+
+# CONSTS
+protocol_dir = os.path.dirname(os.path.abspath(__file__))+"/**/*.rtf"
+
+
+def handle_files():
+    '''
+    The main function to start processing the rtf files
+    into csv
+    '''
+    name = []
+    file_prefix = "\old_committee_"
+    for file_name in glob2.glob(protocol_dir):
+        if file_prefix in file_name:
+            doc = Rtf15Reader.read(open(file_name))
+            data = PlaintextWriter.write(doc).getvalue()
+            data = data.split(':')
+            for row in data:
+                if "מוזמנים" in row:
+                    index_visitor = data.index(row) + 1
+            name.append({'header': 'מוזמנים', 'body': data[index_visitor]})
+    pass
+
+
+
+# def write_to_files(peoples, people_to_jobs_clean):
+#     with open('full_people_list.csv', 'w') as f:
+#         w = csv.writer(f)
+#         w.writerow(base_values)  # field header
+#         w.writerows([list(people) for people in peoples])
+#
+#     with open('person_to_positions.csv', 'w') as f:
+#         w = csv.writer(f)
+#         w.writerow(["full_name", "positions"])  # field header
+#         w.writerows([c for c in people_to_jobs_clean.items()])
+
+
+def main():
+    handle_files()
+    # write_to_files(peoples, people_to_jobs_clean)
+    pass
+
+
+if __name__ == '__main__':
+    main()
